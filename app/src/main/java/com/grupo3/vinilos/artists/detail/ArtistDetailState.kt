72969490e8fc7a0c8675dc470c33bf64444473f9
dfhernandezp12 <142235794+dfhernandezp12@users.noreply.github.com>
2024-05-07 21:29:18 -0500

    Add artist detail front

---
+++ app/src/main/java/com/grupo3/vinilos/artists/detail/ArtistDetailState.kt
@@ -0,0 +1,8 @@
+package com.grupo3.vinilos.artists.detail
+
+import com.grupo3.vinilos.artists.dto.ArtistDto
+
+data class ArtistDetailState (
+    val artist: ArtistDto =ArtistDto(0,"","","", null, null),
+    val errorMessage: String? = null
+)
