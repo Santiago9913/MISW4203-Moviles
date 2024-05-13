7501d9bb0c1cbe758c4e21010fcdeb3d34765dba
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2024-05-12 21:54:28 -0500

    Sprint 1 (#54) (#66)
    * Feat/skeleton (#44)
    
    * init
    
    * skeleton init
    
    * add color scheme
    
    * Artist list ui implemented (#45)
    
    * Feat/artists view model (#46)
    
    * Artist list ui implemented
    
    * feat(artists): add logic to fetch data
    
    * sort artists list (#48)
    
    * sort artists list
    
    * use one variable for sort
    
    * add uiPadding as global object (#47)
    
    * Add Albums service and implementation (#49)
    
    * Add Albums service and implementation
    
    * Update RetroFitInstance.kt
    
    * Feature/ #11 UI implementation for albums (#50)
    
    * #11
    Fist implementation of UI
    
    * #11
    Fixing related issues when displaying no data.
    
    * #11
    PR comments
    
    * Agrega navegacion hacia detalle de un artista (#51)
    
    * Readme added
    
    * add prerequisites for cold starts
    
    * Feature/10 pruebas e2e navegacion detalle artista (#53)
    
    Implementa pruebas e2e
    
    ---------
    
    Co-authored-by: dfhernandezp12 <df.hernandezp12@uniandes.edu.co>
    Co-authored-by: Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
    Co-authored-by: Juan Carlos De Jesus <j.dejesus@uniandes.edu.co>

--- app/src/main/java/com/grupo3/vinilos/ui/theme/Color.kt
+++ app/src/main/java/com/grupo3/vinilos/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.grupo3.vinilos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 import androidx.core.graphics.toColorInt
+import androidx.core.graphics.toColorInt
 
 val Primary = Color("#0DA5FB".toColorInt())
 val Secondary = Color("#B4AAD5".toColorInt())
