e93f5c4f385d79d7756542c8da581bbe327c8064
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-05-17 08:33:20 -0500

    feat: se agrega Share preferences para gestion de usuarios al momento de login (#70)

--- app/src/main/java/com/grupo3/vinilos/utils/Constants.kt
+++ app/src/main/java/com/grupo3/vinilos/utils/Constants.kt
@@ -1,3 +1,5 @@
 package com.grupo3.vinilos.utils
 
 const val ERROR_MESSAGE = "Algo salió mal. Intente de nuevo..."
+const val ES_VISITANTE = "es_visitante"
+const val USER_PREFS = "user_preferences"
