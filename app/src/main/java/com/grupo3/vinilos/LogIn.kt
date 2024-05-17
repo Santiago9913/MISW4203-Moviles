f35880d79441e4859534c38c95d01f40dfb19339
ccpinzonh1 <cc.pinzonh1@uniandes.edu.co>
2024-05-16 23:39:43 -0500

    feat: se agrega Share preferences para gestion de usuarios al momento de login

--- app/src/main/java/com/grupo3/vinilos/LogIn.kt
+++ app/src/main/java/com/grupo3/vinilos/LogIn.kt
@@ -1,5 +1,6 @@
 package com.grupo3.vinilos
 
+import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,35 +12,51 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.grupo3.vinilos.ui.theme.Accent
 import com.grupo3.vinilos.ui.theme.Primary
+import com.grupo3.vinilos.utils.ES_VISITANTE
+import com.grupo3.vinilos.utils.USER_PREFS
 
 @Composable
 fun LogInScreen(
     navController: NavController
-){
+) {
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE)
+
     Column(
         Modifier
             .padding(all = 16.dp)
             .fillMaxHeight()
             .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { navController.navigate("Home") }, colors = ButtonDefaults.buttonColors(
-            containerColor = Primary,
-        ),) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                sharedPreferences.edit().putBoolean(ES_VISITANTE, true).apply()
+                navController.navigate("Home")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Primary,
+            ),
+        ) {
             Text(text = "Visitante")
         }
-       Button(
-           onClick = { navController.navigate("Home?auth=${true}") },
-           colors = ButtonDefaults.buttonColors(
-               containerColor = Accent,
-           ),
-       ) {
-           Text(text = "Coleccionista")
-       }
-        
+        Button(
+            onClick = {
+                sharedPreferences.edit().putBoolean(ES_VISITANTE, false).apply()
+                navController.navigate("Home?auth=${true}")
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Accent,
+            ),
+        ) {
+            Text(text = "Coleccionista")
+        }
+
     }
 }
