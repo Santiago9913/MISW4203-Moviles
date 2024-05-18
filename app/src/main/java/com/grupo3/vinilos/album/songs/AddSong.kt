517c46635fea19ba4643d886c0ee126ade187794
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-05-17 21:48:16 -0500

    feat: app working as expected

--- app/src/main/java/com/grupo3/vinilos/album/songs/AddSong.kt
+++ app/src/main/java/com/grupo3/vinilos/album/songs/AddSong.kt
@@ -2,28 +2,32 @@ package com.grupo3.vinilos.album.songs
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.grupo3.vinilos.R
 import com.grupo3.vinilos.album.detail.AlbumDetailViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.grupo3.vinilos.album.dto.SongCreateDto
+import com.grupo3.vinilos.ui.theme.Accent
+import com.grupo3.vinilos.ui.theme.UiPadding
 import com.grupo3.vinilos.utils.Screen
+import com.grupo3.vinilos.utils.isValidSongDuration
 
 @Composable
 fun AddSong(
@@ -35,16 +39,13 @@ fun AddSong(
     var songDuration by remember { mutableStateOf("") }
     val context = LocalContext.current
     val state by viewModel.state.collectAsState()
-    var initialRender by rememberSaveable { mutableStateOf(true) }
-
-    val successMessage = stringResource(id = R.string.song_successfully_added_label)
 
     LaunchedEffect(state.songs.size) {
-        if (!initialRender && state.songs.isNotEmpty()) {
-            Toast.makeText(context, successMessage, Toast.LENGTH_SHORT).show()
+        if (state.songs.isNotEmpty()) {
+            Toast.makeText(context, R.string.song_successfully_added_label, Toast.LENGTH_LONG)
+                .show()
             navigateTo(Screen.SongList.route.replace("{albumId}", albumId!!))
         }
-        initialRender = false
     }
 
     LaunchedEffect(state.errorMessage) {
@@ -53,29 +54,45 @@ fun AddSong(
         }
     }
 
-    Column {
-        TextField(
+    Column(
+        Modifier.padding(
+            start = UiPadding.large,
+            top = UiPadding.medium,
+            end = UiPadding.large
+        )
+    ) {
+        OutlinedTextField(
             value = songName,
             onValueChange = { songName = it },
             label = { Text(stringResource(id = R.string.input_song_name_label)) },
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(onNext = { /* Handle next action */ })
+            modifier = Modifier.fillMaxWidth()
         )
-        TextField(
+        OutlinedTextField(
             value = songDuration,
             onValueChange = { songDuration = it },
             label = { Text(stringResource(id = R.string.input_song_duration_label)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(onDone = { /* Handle done action */ })
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = "MM:SS") }
         )
-        Button(onClick = {
-            if (albumId != null) {
-                viewModel.addSong(albumId.toInt(), SongCreateDto(songName, songDuration))
-            }
-        }) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            onClick = {
+                if (songName.isBlank() || songDuration.isBlank()) {
+                    Toast.makeText(context, R.string.song_name_duration_error, Toast.LENGTH_SHORT)
+                        .show()
+                } else if (albumId != null && isValidSongDuration(songDuration)) {
+                    viewModel.addSong(albumId.toInt(), SongCreateDto(songName, songDuration))
+                } else {
+                    Toast.makeText(context, R.string.song_duration_format_error, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Accent,
+            ),
+        ) {
             Text(stringResource(id = R.string.save_song_button))
         }
     }
